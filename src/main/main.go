1358326e044394292e305409fe4c629d639b8d68
Yeldos Balgabekov <byeldos@amazon.com>
2019-07-01 18:43:56 +0200

    [08] [hw] adding comments to the main code. removed unnecessary code

--- src/main/main.go
+++ src/main/main.go
@@ -20,33 +20,37 @@ func sampleFunc() error {
 	return fmt.Errorf("error")
 }
 
+func runWorker(tasks chan func() error, errors chan struct{}, maxError int) {
+	// endless worker if not errors limit reached or payload exhausted
+	for {
+		if task, ok := <-tasks; ok {  // if there are some tasks available in the pipeline/channel
+			if len(errors) == maxError {  
+				// if clean the pipeline if maxErrors were occurred to relieve wg.Wait()
+				// i.e. to make sure there are no 'alive' processes awaited by wg.Wait()
+				wg.Done()
+				continue
+			} else if err := task(); err != nil {  // add the channel `errors` if a task is erroneous
+				if len(errors) != maxError {
+					errors <- struct{}{}
+				}
+			}
+			wg.Done()
+		} else {  // if the channel `tasks` has no paylaod anymore
+			break
+		}
+	}
+}
+
 func Run (payload []func()error, maxError int, workerCount int) {
 	tasks := make(chan func() error, len(payload))
 	errors := make(chan struct{}, maxError)
 
-	// kick-off a process in the background to read and execute from the tasks
+	// start the workers in the background to read and execute from the channel `tasks`
 	for i := 0; i < workerCount; i++ {
-		go func() {
-			// endless worker if not errors limit reached or payload exhausted
-			for {
-				if task, ok := <-tasks; ok {
-					if len(errors) == maxError {
-						wg.Done()
-						continue
-					} else if err := task(); err != nil {
-						if len(errors) != maxError {
-							errors <- struct{}{}
-						}
-					}
-					wg.Done()
-				} else {
-					break
-				}
-			}
-		}()
+		go runWorker(tasks, errors, maxError)
 	}
 
-	// start pushing the payload to the tasks
+	// start pushing the payload to the channel `tasks`
 	// such that the workers above would finish the payload
 	for _, task := range payload {
 		wg.Add(1)
@@ -60,8 +64,8 @@ func Run (payload []func()error, maxError int, workerCount int) {
 }
 
 func main() {
-	maxError := 1
-	workerCount := 2
+	maxError := 2
+	workerCount := 3
 	var payload []func()error
 	for i := 0; i < 15; i++ {
 		payload = append(payload, sampleFunc)
